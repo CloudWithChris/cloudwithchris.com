fd7b9fedca68426efd435f84ba30199a27b65282
Chris Reddington <791642+chrisreddington@users.noreply.github.com>
2021-06-09 08:45:52 +0100

    Additions for cloudfront and cert
    Signed-off-by: Chris Reddington <791642+chrisreddington@users.noreply.github.com>

--- terraform/modules/aws-stamp/main.tf
+++ terraform/modules/aws-stamp/main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  version = "~> 1.0"
+  region = "us-east-1"
+  alias = "us-east-1"
+}
+
 variable "environment" {
   type = string
 }
@@ -25,7 +31,7 @@ data "azurerm_dns_zone" "cloudwithchris" {
 }
 
 resource "aws_s3_bucket" "main_stg" {
-  bucket = "${local.resource_prefix_no_dashes}"
+  bucket = local.resource_prefix_no_dashes
   acl    = "public-read"
 
   website {
@@ -56,10 +62,80 @@ resource "aws_s3_bucket_policy" "main_stg_policy" {
   })
 }
 
-resource "azurerm_dns_cname_record" "aws_record" {
+resource "aws_cloudfront_origin_access_identity" "example" {
+  comment = "Some comment"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.main_stg.name}.${aws_s3_bucket.main_stg.website_domain}"
+    origin_id = local.resource_prefix_no_dashes
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.example
+    }
+  }
+
+  is_ipv6_enabled     = true
+  enabled             = true
+  default_root_object = "index.html"
+
+  aliases = ["${var.environment}.aws.cloudwithchris.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.resource_prefix_no_dashes
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.cert.arn
+    ssl_support_method = "sni-only"
+  }
+
+  tags = tags
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "${environment}.cloudwithchris.com"
+  validation_method = "DNS"
+  provider          = "aws.us-east-1" # <== Add this
+
+  tags              = tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "azurerm_dns_cname_record" "cloudfront_dns_bind" {
   name                = "${var.environment}.aws"
   zone_name           = data.azurerm_dns_zone.cloudwithchris.name
   resource_group_name = var.core_resource_group_name
   ttl                 = 300
-  record              = "${aws_s3_bucket.main_stg.name}.${aws_s3_bucket.main_stg.website_domain}"
+  record              = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
+resource "azurerm_dns_cname_record" "ssl_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name                = each.value.name
+  zone_name           = data.azurerm_dns_zone.cloudwithchris.name
+  resource_group_name = var.core_resource_group_name
+  record              = each.value.record
+  ttl                 = 300
 }
