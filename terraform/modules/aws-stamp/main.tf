807dfddc27095c037dc8f47b5d7f6e3d42d2f886
Chris Reddington <791642+chrisreddington@users.noreply.github.com>
2021-06-09 11:32:01 +0100

    Adjustments
    Signed-off-by: Chris Reddington <791642+chrisreddington@users.noreply.github.com>

--- terraform/modules/aws-stamp/main.tf
+++ terraform/modules/aws-stamp/main.tf
@@ -20,29 +20,29 @@ variable "tags" {
 }
 
 locals {
-  resource_prefix_no_dashes = replace(var.resource_prefix, "-", "")
+  resource_prefix_no_dashes   = replace(var.resource_prefix, "-", "")
 }
 
 
 data "azurerm_dns_zone" "cloudwithchris" {
-  name                = "cloudwithchris.com"
-  resource_group_name = var.core_resource_group_name
+  name                        = "cloudwithchris.com"
+  resource_group_name         = var.core_resource_group_name
 }
 
 resource "aws_s3_bucket" "main_stg" {
-  bucket = local.resource_prefix_no_dashes
-  acl    = "public-read"
+  bucket                      = local.resource_prefix_no_dashes
+  acl                         = "public-read"
 
   website {
-    index_document = "index.html"
-    error_document = "error.html"
+    index_document            = "index.html"
+    error_document            = "error.html"
   }
 
-  tags = var.tags
+  tags                        = var.tags
 }
 
 resource "aws_s3_bucket_policy" "main_stg_policy" {
-  bucket = aws_s3_bucket.main_stg.id
+  bucket                      = aws_s3_bucket.main_stg.id
 
   policy = <<EOF
 {
@@ -63,7 +63,7 @@ EOF
 }
 
 resource "aws_cloudfront_origin_access_identity" "example" {
-  comment = "Some comment"
+  comment                     = "Some comment"
 }
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
@@ -71,58 +71,58 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     aws_acm_certificate_validation.cert_validation
   ]
   origin {
-    domain_name = aws_s3_bucket.main_stg.website_endpoint
-    origin_id = local.resource_prefix_no_dashes
+    domain_name               = aws_s3_bucket.main_stg.website_endpoint
+    origin_id                 = local.resource_prefix_no_dashes
 
     custom_origin_config {
-      origin_protocol_policy = "http-only"
+      origin_protocol_policy  = "http-only"
       # The protocol policy that you want CloudFront to use when fetching objects from the origin server (a.k.a S3 in our situation). HTTP Only is the default setting when the origin is an Amazon S3 static website hosting endpoint, because Amazon S3 doesn’t support HTTPS connections for static website hosting endpoints.
-      http_port            = 80
-      https_port           = 443
-      origin_ssl_protocols = ["TLSv1.2", "TLSv1.1", "TLSv1"]
+      http_port               = 80
+      https_port              = 443
+      origin_ssl_protocols    = ["TLSv1.2", "TLSv1.1", "TLSv1"]
     }
   }
 
-  is_ipv6_enabled     = true
-  enabled             = true
-  default_root_object = "index.html"
+  is_ipv6_enabled             = true
+  enabled                     = true
+  default_root_object         = "index.html"
 
-  aliases = ["${var.environment}.aws.cloudwithchris.com"]
+  aliases                     = ["${var.environment}.aws.cloudwithchris.com"]
 
   restrictions {
     geo_restriction {
-      restriction_type = "none"
+      restriction_type        = "none"
     }
   }
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.resource_prefix_no_dashes
+    allowed_methods           = ["GET", "HEAD"]
+    cached_methods            = ["GET", "HEAD"]
+    target_origin_id          = local.resource_prefix_no_dashes
 
     forwarded_values {
-      query_string = false
+      query_string            = false
 
       cookies {
-        forward = "none"
+        forward               = "none"
       }
     }
 
-    viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy    = "redirect-to-https"
   }
 
   viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.cert.arn
-    ssl_support_method = "sni-only"
+    acm_certificate_arn       = aws_acm_certificate.cert.arn
+    ssl_support_method        = "sni-only"
   }
 
   #tags = tags
 }
 
 resource "aws_acm_certificate" "cert" {
-  domain_name       = "${var.environment}.aws.cloudwithchris.com"
-  validation_method = "DNS"
-  provider          = aws.us-east-1 # <== Add this
+  domain_name                 = "${var.environment}.aws.cloudwithchris.com"
+  validation_method           = "DNS"
+  provider                    = aws.us-east-1 # <== Add this
 
   #tags              = tags
 
@@ -132,31 +132,31 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "azurerm_dns_cname_record" "cloudfront_dns_bind" {
-  name                = "${var.environment}.aws"
-  zone_name           = data.azurerm_dns_zone.cloudwithchris.name
-  resource_group_name = var.core_resource_group_name
-  ttl                 = 300
-  record              = aws_cloudfront_distribution.s3_distribution.domain_name
+  name                        = "${var.environment}.aws"
+  zone_name                   = data.azurerm_dns_zone.cloudwithchris.name
+  resource_group_name         = var.core_resource_group_name
+  ttl                         = 300
+  record                      = aws_cloudfront_distribution.s3_distribution.domain_name
 }
 
 resource "azurerm_dns_cname_record" "ssl_validation" {
   for_each = {
     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
+      name                    = dvo.resource_record_name
+      record                  = dvo.resource_record_value
+      type                    = dvo.resource_record_type
     }
   }
 
-  name                = trimsuffix(each.value.name, ".")
-  zone_name           = data.azurerm_dns_zone.cloudwithchris.name
-  resource_group_name = var.core_resource_group_name
-  record              = each.value.record
-  ttl                 = 300
+  name                        = trimsuffix(each.value.name, ".")
+  zone_name                   = trimsuffix(data.azurerm_dns_zone.cloudwithchris.name, ".cloudwithchris.com")
+  resource_group_name         = var.core_resource_group_name
+  record                      = each.value.record
+  ttl                         = 300
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in azurerm_dns_cname_record.ssl_validation : record.fqdn]
-  provider          = aws.us-east-1 # <== Add this
+  provider                    = aws.us-east-1 # <== Add this
+  certificate_arn             = aws_acm_certificate.cert.arn
+  validation_record_fqdns     = [for record in azurerm_dns_cname_record.ssl_validation : record.fqdn]
 }
