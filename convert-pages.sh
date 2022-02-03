808945383988ea3fe68bee1be0689d25646b3570
Chris Reddington <791642+chrisreddington@users.noreply.github.com>
2022-02-03 15:18:48 +0000

    Script adjustment

--- convert-pages.sh
+++ convert-pages.sh
@@ -11,7 +11,6 @@ for subfolder in ${subdirectories[@]}; do
   # Find all markdown files in this directory that aren't _index.md
   for f in $(find "./content/${subfolder}" -name '*.md' | sort ); do
 
-
     # For each of them - 
     # 1. Get the path of the file (string before filename)
     # 2. Get the filename (just the name of the file.md)
@@ -29,16 +28,21 @@ for subfolder in ${subdirectories[@]}; do
       echo "Directory: ${directory} Section: ${section} File: ${file_name} New Folder: ${folder_name}"
 
       # Create a folder to replace the markdown file
-      #mkdir "${directory}/${folder_name}"
-      #mkdir "${directory}/${folder_name}/images"
-      #cp $f "${directory}/${folder_name}/index.md"
+      mkdir "${directory}/${folder_name}"
+      mkdir "${directory}/${folder_name}/images"
+      cp $f "${directory}/${folder_name}/index.md"
 
       if [ -d $images_folder ]; then
         echo "${section} ${folder_name} exists"
         mv "${images_folder}/*" "${directory}/${folder_name}/images"
       fi
 
-      #rm $f
+      if [ -f $image_in_static_folder ]; then
+        echo "${section} ${image_in_static_folder} exists"
+        mv "${image_in_static_folder}/*" "${directory}/${folder_name}/images"
+      fi
+
+      rm $f
     fi
 
     
