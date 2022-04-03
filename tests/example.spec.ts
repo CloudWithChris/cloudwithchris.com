dfa4ef4eba4f4521c6b2cb2bf35f7d11ee8f5f4c
Chris Reddington <791642+chrisreddington@users.noreply.github.com>
2022-04-03 21:28:48 +0100

    Tests based on JSON LD

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -36,6 +36,7 @@ function getFiles(dir, filelist) {
       filelist.push(
         {
           filename: dir.replace('content/',''),
+          url: new URL(dir.replace('content/',''), 'https://www.cloudwithchris.com').toString(),
           title: object.title,
           description: object.description
         }
@@ -49,35 +50,101 @@ function getFiles(dir, filelist) {
 
 function testTitle(record){
   // Check that the appropriate title is displayed
-  test(`Check heading is correct: ${record.filename}`, async ({ page }) => {
-    //let directURL = new URL(record.filename, baseURL);
+  // test(`Check heading is correct: ${record.filename}`, async ({ page }) => {
+  //   //let directURL = new URL(record.filename, baseURL);
+  //   await page.goto(record.filename);
+  //   const title = page.locator('h1');
+  //   await expect(title).toHaveText(record.title);
+  // })
+
+  // test(`Check page title is correct: ${record.filename}`, async ({ page }) => {
+  //   //let directURL = new URL(record.filename, baseURL);
+  //   let expectedTitle = record.title.concat(' | Cloud With Chris');
+  //   await page.goto(record.filename);
+  //   const title = page.locator('title');
+  //   await expect(title).toHaveText(expectedTitle);
+  //   const twittertitle = page.locator('meta[name="twitter:title"]');
+  //   await expect(twittertitle).toHaveAttribute("content", expectedTitle);
+  //   const ogtitle = page.locator('meta[property="og:title"]');
+  //   await expect(ogtitle).toHaveAttribute("content", expectedTitle);
+  // })
+
+
+  // test(`Check organization metadata is correct: ${record.filename}`, async ({ page }) => {
+
+  //   // Arrange
+  //   const expectedObject = {
+  //     "@context":"https://schema.org",
+  //     "@type":"Organization",
+  //     "url":"https://www.cloudwithchris.com/",
+  //     "logo":"https://www.cloudwithchris.com/img/cloudwithchrislogo.png"
+  //   }
+
+  //   // Act
+  //   await page.goto(record.filename);
+  //   const actual = page.locator('id=meta-organisation');
+
+  //   // Assertq
+  //   expect(await actual.evaluate(node => JSON.parse(node.innerHTML))).toBe(expectedObject);
+  // })
+
+  // test(`Check website metadata is correct: ${record.filename}`, async ({ page }) => {
+
+  //   // Arrange
+  //   const expectedObject = {
+  //     "@context":"http://schema.org",
+  //     "@type":"WebSite",
+  //     "url":"https://www.cloudwithchris.com/",
+  //     "sameAs": [
+  //       "https://twitter.com/CloudWithChris",
+  //       "https://github.com/CloudWithChris"
+  //     ],
+  //     "name":"Cloud With Chris",
+  //     "logo":"https://www.cloudwithchris.com/favicon.ico",
+  //     "potentialAction": {
+  //       "@type":"SearchAction",
+  //       "target": {
+  //         "@type":"EntryPoint",
+  //         "urlTemplate":"https://www.cloudwithchris.com/search/?s={search_term_string}"
+  //       },
+  //       "query-input":"required name=search_term_string"
+  //     }
+  //   }    
+
+  //   // Act
+  //   await page.goto(record.filename);
+  //   const actual = page.locator('id=meta-website');
+
+  //   // Assert
+  //   expect(await actual.evaluate(node => JSON.parse(node.innerHTML))).toBe(expectedObject);
+  // })
+
+
+  test(`Check Breadcrumbs metadata is correct: ${record.filename}`, async ({ page }) => {
+
+    // Arrange - N/A
+
+    // Act
     await page.goto(record.filename);
-    const title = page.locator('h1');
-    await expect(title).toHaveText(record.title);
+    const actual = page.locator('id=meta-breadcrumbs');
+    const actualObject = await actual.evaluate(node => JSON.parse(node.innerHTML));
+
+
+    // Assert
+    expect(actualObject.itemListElement.length).toBeGreaterThanOrEqual(2);
+    expect(actualObject.itemListElement[actualObject.itemListElement.length - 1].name).toBe(record.title);
   })
 
-  test(`Check page title is correct: ${record.filename}`, async ({ page }) => {
-    //let directURL = new URL(record.filename, baseURL);
-    let expectedTitle = record.title.concat(' | Cloud With Chris');
-    await page.goto(record.filename);
-    const title = page.locator('title');
-    await expect(title).toHaveText(expectedTitle);
-    const twittertitle = page.locator('meta[name="twitter:title"]');
-    await expect(twittertitle).toHaveAttribute("content", expectedTitle);
-    const ogtitle = page.locator('meta[property="og:title"]');
-    await expect(ogtitle).toHaveAttribute("content", expectedTitle);
-  })
-
-  test(`Check page has description metadata: ${record.filename}`, async ({ page }) => {
-    //let directURL = new URL(record.filename, baseURL);
-    await page.goto(record.filename);
-    const description = page.locator('meta[name="description"]');
-    await expect(description).toHaveAttribute("content", record.description);
-    const twitterdescription = page.locator('meta[name="twitter:description"]');
-    await expect(twitterdescription).toHaveAttribute("content", record.description);
-    const ogdescription = page.locator('meta[property="og:description"]');
-    await expect(ogdescription).toHaveAttribute("content", record.description);
-  })
+  // test(`Check page has description metadata: ${record.filename}`, async ({ page }) => {
+  //   //let directURL = new URL(record.filename, baseURL);
+  //   await page.goto(record.filename);
+  //   const description = page.locator('meta[name="description"]');
+  //   await expect(description).toHaveAttribute("content", record.description);
+  //   const twitterdescription = page.locator('meta[name="twitter:description"]');
+  //   await expect(twitterdescription).toHaveAttribute("content", record.description);
+  //   const ogdescription = page.locator('meta[property="og:description"]');
+  //   await expect(ogdescription).toHaveAttribute("content", record.description);
+  // })
 }
 
 // Iterate through the records and run several tests per record
