af5e13d83dd0e3bf3b26afafdac8c90eac01970e
Chris Reddington <791642+chrisreddington@users.noreply.github.com>
2022-06-02 20:17:15 +0100

    Add additional metadata tests for episodes

--- tests/episode.spec.ts
+++ tests/episode.spec.ts
@@ -9,17 +9,54 @@ for (const record of testData) {
   test(`${record.filename} - Episode Metadata Check`, async ({ page }) => {
     // Arrange
     // N/A
+    const baseUrl = record.url.replace(record.filename, "")
 
     // Act
+    /* Episode */
     await page.goto(record.url);
-    const actual = page.locator('id=meta-episode');
-    const actualObject = await actual.evaluate(node => JSON.parse(node.innerHTML));
+    const episodeMetadata = await page.locator('id=meta-episode');
+    const episodeObject = await episodeMetadata.evaluate(node => JSON.parse(node.innerHTML));
+
+    /* Organization */
+    const organizationMetadata = await page.locator('id=meta-organisation');
+    const organizationObject = await organizationMetadata.evaluate(node => JSON.parse(node.innerHTML));
+
+    /* Website */
+    const websiteMetadata = await page.locator('id=meta-website');
+    const websiteObject = await websiteMetadata.evaluate(node => JSON.parse(node.innerHTML));
+
+    /* Breadcrumbs */
+    const breadcrumbsMetadata = await page.locator('id=meta-breadcrumbs');
+    const breadcrumbsObject = await breadcrumbsMetadata.evaluate(node => JSON.parse(node.innerHTML));
 
     // Assert
-    expect(actualObject["@context"]).toBe("http://schema.org");
-    expect(actualObject["@type"]).toBe("PodcastEpisode");
-    expect(actualObject["name"]).toBe(record.title);
-    expect(actualObject["url"]).toBe(record.url);
-    expect(actualObject["description"]).toBe(record.description);
+    /* Episode */
+    expect(episodeObject["@context"]).toBe("http://schema.org");
+    expect(episodeObject["@type"]).toBe("PodcastEpisode");
+    expect(episodeObject["name"]).toBe(record.title);
+    expect(episodeObject["url"]).toBe(record.url);
+    expect(episodeObject["description"]).toBe(record.description);
+
+    /* Organization */
+    expect(organizationObject["@context"]).toBe("https://schema.org");
+    expect(organizationObject["@type"]).toBe("Organization");
+    expect(organizationObject["url"]).toBe(baseUrl);
+    
+    // Add an assertion on the logo here
+
+    /* Website */
+    expect(websiteObject["@context"]).toBe("http://schema.org");
+    expect(websiteObject["@type"]).toBe("WebSite");
+    expect(websiteObject["name"]).toBe(record.title);
+    expect(websiteObject["sameAs"][0]).toBe("https://twitter.com/reddobowen");
+    expect(websiteObject["sameAs"][1]).toBe("https://github.com/CloudWithChris");
+
+    /* Breadcrumbs */
+    expect(breadcrumbsObject["@context"]).toBe("https://schema.org");
+    expect(breadcrumbsObject["@type"]).toBe("BreadcrumbList");
+    expect(breadcrumbsObject["itemListElement"][0]["item"]).toBe(baseUrl);
+    expect(breadcrumbsObject["itemListElement"][0]["item"]).toBe(baseUrl);
+    expect(breadcrumbsObject["itemListElement"][1]["name"]).toBe("Episodes");
+    expect(breadcrumbsObject["itemListElement"][2]["name"]).toBe(record.title);
   });
 }
